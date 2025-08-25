a987fff6c2561a4eb8fa3799d8f79a04795c1760
James Mallon <james.h.mallon@gmail.com>
2025-08-25 23:16:36 +1000

    Version 2.0

--- guessing_game.py
+++ guessing_game.py
@@ -1,9 +1,20 @@
 import random
-lower = int(input("Enter the lower bound: "))
-upper = int(input("Enter the upper bound: "))
+print("Welcome to the Guessing Game!")
+print("I'm thinking of a number between two numbers you will provide.")
+
+lower = int(input("Enter the lower number: "))
+while lower < 1: 
+    print("The lower number must be greater than 0.")
+    lower = int(input("Please enter a number greater than 0: "))
+
+upper = int(input("Enter the upper number: "))
+while upper <= lower: 
+    print("The upper number must be greater than the lower number.")
+    upper = int(input(f"Please enter a number greater than {lower}: "))
 
 secret= random.randrange(lower, upper + 1)
 
+print(f"I have selected a number between {lower} and {upper}. Can you guess it?")
 
 while True:
     player_guess = int(input("Enter your quess: "))
