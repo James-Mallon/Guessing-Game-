93bcaddc82a38900e80a40aea35e1287e18c8dfb
James Mallon <james.h.mallon@gmail.com>
2025-08-26 00:43:46 +1000

    Version 3

--- guessing_game.py
+++ guessing_game.py
@@ -1,18 +1,32 @@
 import random
 print("Welcome to the Guessing Game!")
 print("I'm thinking of a number between two numbers you will provide.")
+count = 1
 
-lower = int(input("Enter the lower number: "))
-while lower < 1: 
-    print("The lower number must be greater than 0.")
-    lower = int(input("Please enter a number greater than 0: "))
+while True:
+    lower = input("Enter the lower number: ")
+    try:
+        lower = int(lower)
+        if lower < 1:
+            print("The lower number must be greater than 0.")
+            continue
+        break 
+    except ValueError:
+        print("Please enter a valid whole number.")
+ 
 
-upper = int(input("Enter the upper number: "))
-while upper <= lower: 
-    print("The upper number must be greater than the lower number.")
-    upper = int(input(f"Please enter a number greater than {lower}: "))
+while True:
+    upper = input("Enter the upper number: ")
+    try:
+        upper = int(upper)
+        if upper <= lower:
+            print(f"The upper number must be greater than the lower number ({lower}).")
+            continue
+        break
+    except ValueError:
+        print("Please enter a valid whole number greater than the lower limit.")
 
-secret= random.randrange(lower, upper + 1)
+secret = random.randrange(lower, upper + 1)
 
 print(f"I have selected a number between {lower} and {upper}. Can you guess it?")
 
@@ -20,10 +34,11 @@ while True:
     player_guess = int(input("Enter your quess: "))
 
     if player_guess == secret: 
-        print ("You win!")
+        print (f"You won in {count} guesses!")
         input("Press Key to exit")
         break
     else:
+        count += 1
         if int(player_guess) < secret:
             print("Your quess is too low!")
         else:
